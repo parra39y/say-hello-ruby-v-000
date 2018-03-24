a6186e57f26569bb8d9ef6bbf0385199fd0c578f
Yesenia Parra <parra22y@mtholyoke.edu>
2018-03-24 20:18:59 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 
-describe "say_hello" do 
-  
+describe "say_hello" do
+
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Gabriela!")
     say_hello("Gabriela")
